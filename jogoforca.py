7604b778d41d36737110955f33a69fb7782bff98
vinixavier123 <viniciusxdelima@gmail.com>
2024-12-31 18:50:35 -0300

    fix version 1.1

--- jogoforca.py
+++ jogoforca.py
@@ -1,4 +1,4 @@
-#Projeto da Data Science Academy
+# Projeto da Data Science Academy
 
 import random
 from os import system, name
@@ -6,14 +6,51 @@ from os import system, name
 def limpar():
     if name == "nt":
         _ = system('cls')
-    else: _ = system('clear')
+    else:
+        _ = system('clear')
     
 def jogo():
     limpar()
     print("\nBem-Vindo(a) ao nosso jogo da forca!\n")
     print("Adivinhe a palavra abaixo: ")
     
-    with open('palavras_forca.txt', 'r', encoding='utf-8') as arquivo:
+    with open('C:/Users/User/Documents/PROJETOS/PYTHON/Jogo da Forca/palavras_forca.txt', 'r', encoding='utf-8') as arquivo:
         palavras = [linha.strip() for linha in arquivo]
-    palavra = random.choice(palavras)
     
+    # Escolher uma palavra aleatória
+    palavra = random.choice(palavras).lower()
+    letras_descobertas = ["_" for _ in palavra]
+    chances = 5
+    letras_erradas = []
+      
+    while chances > 0:
+        print("\n" + " ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", ", ".join(letras_erradas))
+        
+        tentativa = input("\nDigite uma letra: ").lower()
+        
+        if tentativa in palavra:
+            index = 0
+            for l in palavra:
+                if tentativa == l:
+                    letras_descobertas[index] = l
+                index +=1
+        else: 
+            chances -= 1
+            letras_erradas.append(tentativa)
+            
+        if "_" not in letras_descobertas:
+            print("Parabéns, você venceu! A palavra era:", palavra)
+            break
+        
+    if "_" in letras_descobertas:
+        print("Que pena, você perdeu, a palavra era: ", palavra)
+        denovo = input("Gostaria de tentar novamente: S/N:")
+        if denovo == "S":
+                jogo()
+        else: 
+            print("Obrigado por ter jogado o nosso")
+        
+if __name__ == "__main__":
+    jogo()
