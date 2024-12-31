8162b1e6dffd307ab455d981e358d8556207daf5
vinixavier123 <viniciusxdelima@gmail.com>
2024-12-31 18:25:43 -0300

    fix palavras_forca

--- jogoforca.py
+++ jogoforca.py
@@ -7,3 +7,13 @@ def limpar():
     if name == "nt":
         _ = system('cls')
     else: _ = system('clear')
+    
+def jogo():
+    limpar()
+    print("\nBem-Vindo(a) ao nosso jogo da forca!\n")
+    print("Adivinhe a palavra abaixo: ")
+    
+    with open('palavras_forca.txt', 'r', encoding='utf-8') as arquivo:
+        palavras = [linha.strip() for linha in arquivo]
+    palavra = random.choice(palavras)
+    
